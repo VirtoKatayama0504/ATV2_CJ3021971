5542149705dfcd5a1f7d64cc8fba8c667e5f418f
VirtoKatayama0504 <katayama.v@aluno.ifsp.edu.br>
2024-02-28 11:14:24 -0300

    Exercicios 1-10

--- ATV2_CJ3021971/Program.cs
+++ ATV2_CJ3021971/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Exercicio 1");
+            /*Console.WriteLine("Exercicio 1");
             Console.WriteLine("Digite seu salário: ");
             int salario = int.Parse(Console.ReadLine());
             float salariob = ((salario / 100) * 25) + salario;
@@ -34,10 +34,59 @@
             float dia4 = float.Parse(Console.ReadLine());
             float patrimonio4 = ((patrimonio3 / 100) * dia4) + patrimonio3;
             Console.WriteLine("O resultado final do investimento é igual a {0}", patrimonio4);
-
-
             
-             
+            Console.WriteLine("Exercicio 4");
+            Console.WriteLine("Digite um valor em metros");
+            int m = int.Parse(Console.ReadLine());
+            int c = m * 100;
+            int mm = m * 1000;
+            Console.WriteLine("O valor em centimetros é igual a {0},", c);
+            Console.WriteLine("O valor em milimetros é igual a {0}, ", mm);
+            
+            Console.WriteLine("Exercicio 5");
+            Console.WriteLine("Digite um valor em graus Fahrenheit: ");
+            float f = float.Parse(Console.ReadLine());
+            float c = (f - 32) * (9/5);
+            Console.WriteLine("O valor em Celsius é:{0} ",c);
+            Console.WriteLine("A razao do enunciado dá 0 pois a fração esta invertida");
+
+            Console.WriteLine("Exercicio 6");
+            Console.WriteLine("Digite seu peso em kg: ");
+            int peso = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite sua estatura em metros");
+            float altura = float.Parse(Console.ReadLine());
+            float imc = peso / (altura * altura);
+            Console.WriteLine("O seu indice do imc é igual a {0}", imc);
+            
+            Console.WriteLine("Exercicio 7");
+            Console.WriteLine("Digite o primeiro numero: ");
+            float n1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digiteo segundo numero: ");
+            float n2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o terceiro numero: ");
+            float n3 = float.Parse(Console.ReadLine());
+            float media = (n1 + n2 + n3) / 3;
+            Console.WriteLine("A media ponderada desses 3 numeros é igual a {0}", media);  
+            
+            Console.WriteLine("Exercicio 8");
+            Console.WriteLine("Digite a variação do tempo: ");
+            float tempo = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a distancia: ");
+            float espaco = float.Parse(Console.ReadLine());
+            float escalar = espaco / tempo;
+            Console.WriteLine("A velociadade escalar media é igual a: {0}", escalar);
+            
+            Console.WriteLine("Exercicio 9");
+            Console.WriteLine("Digite um valor em reais: ");
+            float real = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a cotação do dolar: ");
+            float cota = float.Parse(Console.ReadLine());
+            float dolar = real * cota;
+            Console.WriteLine("O valor em dolares é igual a {0}", dolar);
+            */
+            Console.WriteLine("Exercicio 10");
+            Console.WriteLine("Digite um numero de 4 digitos");
+            float n1 = float.Parse(Console.ReadLine());
 
         }
     }
